11c3b031a5e955de7fa11f9e0583381605000252
Amir <nik.amirsa@gmail.com>
2020-07-12 16:38:26 +0430

    connect view to controller

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function index() {
+        return view('welcome');
+    }
+
+    public function about(){
+        return view('pages/about');
+    }
+
+    public function contact(){
+        return view('pages/contact');
+    }
 }
