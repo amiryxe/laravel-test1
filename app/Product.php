3f636aa1a3e4aacb02688ae51ff0b13ce8dd59ef
Amir <nik.amirsa@gmail.com>
2020-07-15 17:56:47 +0430

    add fillable fields for create method

--- app/Product.php
+++ app/Product.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $fillable = ['name', 'description'];
+
+//    protected $hidden = ['name'];
+
     public static function filteredProducts(){
         return self::where('name', 'like', '%P%' )->get();
     }
