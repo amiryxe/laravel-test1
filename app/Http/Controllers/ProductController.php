f09e8ff1c84efd7c9ac18433a3cf2216745c501b
Amir <nik.amirsa@gmail.com>
2020-07-15 17:13:48 +0430

    set model to ProductController

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index() {
+        //  $products = App\Product::orderBy('name')->get();
+        $products = Product::all();
+
+        return view('products.all', compact('products'));
+    }
+
+    public function filtered() {
+        $productFiltered = Product::filteredProducts();
+
+        return view('products.filtered', compact('productFiltered'));
+    }
+
+    public function show($id) {
+        $singleProduct = Product::where('id', '=', $id)->first();
+
+        return view('products.singleProduct', compact('singleProduct'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,23 +18,8 @@ Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/contact', 'PagesController@contact');
 
-Route::get('/products', function (){
-//    $products = App\Product::orderBy('name')->get();
-    $products = Product::all();
-
-    return view('products.all', compact('products'));
-});
-
-Route::get('/products/filtered/', function (){
-    $productFiltered = Product::filteredProducts();
-
-    return view('products.filtered', compact('productFiltered'));
-});
-
-Route::get('/products/{id}', function ($id) {
-    $singleProduct = Product::where('id', '=', $id)->first();
-
-    return view('products.singleProduct', compact('singleProduct'));
-});
+Route::get('/products', 'ProductController@index');
+Route::get('/products/filtered/', 'ProductController@filtered');
+Route::get('/products/{id}', 'ProductController@show');
 
 
